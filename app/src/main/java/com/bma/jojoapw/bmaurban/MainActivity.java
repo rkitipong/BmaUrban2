9d07a4f19e5eb722348cf951d8abc4822aaa16e1
rkitipong <rkitipong@yahoo.com>
2016-08-02 10:18:44 +0700

    Initial commit

---
+++ app/src/main/java/com/bma/jojoapw/bmaurban/MainActivity.java
@@ -0,0 +1,22 @@
+package com.bma.jojoapw.bmaurban;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private WebView mWebView;
+//    private WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mWebView = (WebView) findViewById(R.id.webview);
+//        webSettings = mWebView.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//        mWebView.loadUrl("http://cmc.bangkok.go.th/bmaurban/");
+    }
+}
